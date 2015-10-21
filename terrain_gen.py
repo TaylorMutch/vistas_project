b50ec82075ad34e399454f14dea10e8ca2c9aee7
TaylorMutch <taylormutch@gmail.com>
2015-10-21 14:47:54 -0700

    Added validation to terrain_gen

--- terrain_gen.py
+++ terrain_gen.py
@@ -21,19 +21,27 @@ def main():
         if not input(overwrite_message).lower().startswith('y'):
             sys.exit(-1)
 
-    lng1 = float(input('West Longitude: '))
-    lng2 = float(input('East Longitude: '))
-    lat1 = float(input('North Latitude: '))
-    lat2 = float(input('South Latitude: '))
-    numlats = numlngs = int(input('Number of segments: '))
+    lat1 = float(input('South Latitude: '))
+    lat2 = float(input('North Latitude: '))
+    lng1 = float(input('East Longitude: '))
+    lng2 = float(input('West Longitude: '))
+    numlngs = int(input('Number of segments: '))
+    numlats = int(input('Number of latitude segments (-1 for equal aspect ratio): '))
 
-    payload = {'lat1': str(lat1),
-               'lat2': str(lat2),
-               'lng1': str(lng1),
-               'lng2': str(lng2),
-               'numlats': str(numlats),
-               'numlngs': str(numlngs)}
-    max_retries = 3
+    if (numlats == -1):
+        payload = {'lat1': str(lat1),
+                   'lat2': str(lat2),
+                   'lng1': str(lng1),
+                   'lng2': str(lng2),
+                   'numlngs': str(numlngs)}
+    else:
+        payload = {'lat1': str(lat1),
+                   'lat2': str(lat2),
+                   'lng1': str(lng1),
+                   'lng2': str(lng2),
+                   'numlats': str(numlats),
+                   'numlngs': str(numlngs)}
+
     try:
         r = requests.get(server, params=payload)
         print(r.content)
@@ -44,7 +52,20 @@ def main():
 
     max = 0
 
+    if (numlats == -1): # Get the correct number of latitudes from the server
+        form_feed = b'\x0c'
+        index = 0
+        myChar = b''
+        while myChar != form_feed:
+            myChar = r.content[index:index+1]
+            if myChar == form_feed:
+                break
+            index +=1
+        numlats = int(r.content[:index].decode('ascii').split()[5]) # this is the right position for numlats
+
+
     with open(output_path, 'wb') as f_out:
+        index = 0
         for i in range(1, numlats + 1):
             index = len(r.content) - i*4*numlngs  # floats are 4 bytes long
             floats = struct.unpack('f' * numlngs, r.content[index: index + 4*numlngs])
